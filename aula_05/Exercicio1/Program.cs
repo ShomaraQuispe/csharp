c447253613a9f767ab99e4119679a840cb7c0320
ShomaraQuispe <shomaraclaudia@gmail.com>
2023-08-28 16:08:22 -0300

    Exercicios aula 05

---
+++ aula_05/Exercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor1 = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
+            int procurarNumero;
+
+            Console.WriteLine("Digite o número que você deseja encontrar: ");
+            procurarNumero = Convert.ToInt32(Console.ReadLine());
+
+            for (int indice = 0; indice < vetor1.Length; indice++)
+            {
+                if (procurarNumero == vetor1[indice])
+                {
+                    Console.WriteLine($"O número {vetor1[indice]} está localizado na posição: [{indice}]");
+                }
+            }
+
+            if (procurarNumero > 10 || procurarNumero < 0)
+            {
+                Console.WriteLine($"O número {procurarNumero} não foi encontrado");
+            }
+        }
+    }
+}
---
+++ aula_05/Exercicio3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz02 = new int[3, 3];
+            int somaDiagonalPrincipal, somaDiagonalSecundaria;
+
+            for (int indiceLinha = 0; indiceLinha < Math.Sqrt(matriz02.Length); indiceLinha++)
+            {
+                for (int indiceColuna = 0; indiceColuna < Math.Sqrt(matriz02.Length); indiceColuna++)
+                {
+                    Console.Write($"matriz02[{indiceLinha}, {indiceColuna}] = ");
+                    matriz02[indiceLinha, indiceColuna] = Convert.ToInt32(Console.ReadLine());                 
+                }
+            }
+
+            Console.WriteLine("Diagonal principal: ");
+            for (int indiceLinha = 0; indiceLinha < Math.Sqrt(matriz02.Length); indiceLinha++)
+            {
+                Console.Write($"{matriz02[indiceLinha, indiceLinha]} ");
+            }
+
+            Console.WriteLine("\n\nDiagonal secundária: ");
+            for (int indiceLinha = 0; indiceLinha < Math.Sqrt(matriz02.Length); indiceLinha++)
+            {
+                Console.Write($"{matriz02[indiceLinha, 3 - 1 - indiceLinha]} ");
+            }
+
+            somaDiagonalPrincipal = matriz02[0,0] + matriz02[1, 1] + matriz02[2, 2];
+            somaDiagonalSecundaria = matriz02[2,0] + matriz02[1, 1] + matriz02[0, 2];
+            Console.WriteLine($"\nSoma dos elementos da Diagonal principal: {somaDiagonalPrincipal}");
+            Console.WriteLine($"\nSoma dos elementos da Diagonal segundaria:{somaDiagonalSecundaria}");
+        }
+    }
+}

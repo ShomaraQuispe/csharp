3ac9feaf714cd4b5d8a07f61af4b1176460d6b48
ShomaraQuispe <shomaraclaudia@gmail.com>
2023-08-29 21:39:29 -0300

    Exercícios não-obrigatórios da aula 6 adicionado

---
+++ aula_06/Exercicio2-Collections/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio2_Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroSolicitado, numeroEncontrado;
+            List<int> lista = new List<int>() {2,5,1,3,4,9,7,10,6};
+           
+            Console.WriteLine("Digite o número que você deseja encontrar:");
+            numeroSolicitado = Convert.ToInt32(Console.ReadLine());
+            numeroEncontrado = lista.IndexOf(numeroSolicitado);
+
+            if (numeroEncontrado >= 0)
+                Console.WriteLine($"Posição do número {numeroSolicitado} é: " + lista.IndexOf(numeroSolicitado));
+            else Console.WriteLine($"O numero {numeroSolicitado} não foi encontrado.");
+        }
+    }
+}
---
+++ aula_06/Exercicio4-Collections/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio4_Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> lista = new HashSet<int>() { 2,5,1,3,4,9,7,8,10,6};
+            int numeroSolicitado;
+
+            Console.WriteLine("Digite o número que você deseja encontrar:");
+            numeroSolicitado = Convert.ToInt32(Console.ReadLine());
+
+            if (lista.Contains(numeroSolicitado))
+            {
+                Console.WriteLine($"O número {numeroSolicitado} foi encontrado!");
+            }
+            else
+                Console.WriteLine($"O número {numeroSolicitado} não foi encontrado!");
+        }
+    }
+}
